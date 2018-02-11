9a0765f6ac43a7831a5b05b8ea7d6ffb7c5d225a
lyhlg <lyhlg0201@gmail.com>
2018-02-12 08:57:38 +0900

    modify something

--- havit-web-frontend/src/components/Common/Header.js
+++ havit-web-frontend/src/components/Common/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../../styles/Common/Header.scss';
+import 'styles/css/Common/Header.css';
 
 class Header extends Component {
   render() {
