975c99341cf7aa9938a171915ccd473eeb717251
JungYouTaek <jyt9319@gmail.com>
2018-02-12 20:22:03 +0900

    Add Beauty component

---
+++ havit-web-frontend/src/components/Beauty/Beauty.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Banner } from '../index';
+
+class Beauty extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>전체보기</li>
+          <li>필러</li>
+          <li>보톡스</li>
+          <li>윤곽</li>
+          <li>리프팅</li>
+        </ul>
+        <main>
+          <Banner />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Beauty;
