36eca8173c8bdd16665949fe08f53e1e1c330577
JungYouTaek <jyt9319@gmail.com>
2018-02-12 20:23:13 +0900

    Edit Login component

--- havit-web-frontend/src/components/Header/Signin.js
+++ havit-web-frontend/src/components/Header/Signin.js
@@ -7,14 +7,19 @@ class Signin extends Component {
       <div>
         <h2>회원정보 입력</h2>
         <h3>이메일</h3>
-        <input type="email"/>
+        <input type="email" />
+        <button>이메일 인증하기</button>
+        <h4>이메일 인증번호</h4>
+        <input type="text" />
         <h3>비밀번호 (6~ 15자)</h3>
-        <input type="password" placeholder="비밀번호"/>
+        <input type="password" placeholder="비밀번호" />
         <h4>영문, 숫자로 6자 이상 입력해주세요.</h4>
         <h3>비밀번호 확인</h3>
-        <input type="password" placeholder="비밀번호 확인"/>
+        <input type="password" placeholder="비밀번호 확인" />
         <h4>동일하게 다시 한 번 입력해주세요.</h4>
-        <button><Link to="/privacy">계정 생성</Link></button>
+        <button>
+          <Link to="/privacy">계정 생성</Link>
+        </button>
         <button>취소</button>
       </div>
     );
--- havit-web-frontend/src/components/Home/Home.js
+++ havit-web-frontend/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Banner } from '../index';
 
 class Home extends Component {
   render() {
     return (
       <main>
-        
+        <Banner />
       </main>
     );
   }
