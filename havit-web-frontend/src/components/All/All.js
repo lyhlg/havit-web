fb0f8291c964ca03238a88536250e0b75485ae6b
JungYouTaek <jyt9319@gmail.com>
2018-02-12 20:21:29 +0900

    Add All component

---
+++ havit-web-frontend/src/components/All/All.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Banner } from '../index';
+
+class All extends Component {
+  render() {
+    return (
+      <main>
+        <Banner />
+      </main>
+    );
+  }
+}
+
+export default All;
