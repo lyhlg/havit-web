7187235cd8b80c527e915b2304a602bca50b0203
lyhlg <lyhlg0201@gmail.com>
2018-02-19 17:35:43 +0900

    Edit product Detail CSS

--- havit-web-frontend/src/components/Common/ProductDetail.js
+++ havit-web-frontend/src/components/Common/ProductDetail.js
@@ -9,22 +9,33 @@ class ProductDetail extends Component {
           <p>홈 > 시술 상품 > 보톡스</p>
         </div>
         <div className="productDetail__info">
-          <div className="productDetail__info__img">
+          <div className="productDetail__img">
             <img
               src="http://dummyimage.com/480x480/00c4e2/fff"
               alt="detail"
               align="left"
             />
           </div>
-          <div className="productDetail__info__description">
-            <h5>볼륨 + 탄력</h5>
-            <h3>큐오필 1cc</h3>
-            <h4>신사역</h4>
-            <h4>59000원</h4>
-            <h4>옵션</h4>
-            <button type="submit">찜하기</button>
-            <button type="submit">예약하기</button>
-            <h4>구매 300개</h4>
+          <div className="productDetail__text">
+            <h4 className="productDetail__hospital">[강남구] 라피네 클리닉</h4>
+            <h3 className="productDetail__title">큐오필 1CC</h3>
+            <h5 className="productDetail__description">
+              볼륨+탄력+콜라겐+지속력+피부
+            </h5>
+            <div className="productDetail__sales">
+              <h4 className="productDetail__price">59,000원</h4>
+              <h4 className="productDetail__purchased">
+                <span>38 개 </span>구매
+              </h4>
+            </div>
+            <hr className="productDetail__divider" />
+            <select className="productDetail__option">
+              <option>옵션 선택</option>
+            </select>
+            <div>
+              <button className="productDetail__purchaseBtn"> 구매하기 </button>
+              <button className="productDetail__likeBtn"> 😤 </button>
+            </div>
           </div>
         </div>
       </div>
