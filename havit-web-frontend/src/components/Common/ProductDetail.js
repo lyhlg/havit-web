3b6ca3822450ef98ad1d19196c4000a089895df1
lyhlg <lyhlg0201@gmail.com>
2018-02-19 16:16:38 +0900

    update css

--- havit-web-frontend/src/components/Common/ProductDetail.js
+++ havit-web-frontend/src/components/Common/ProductDetail.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import 'styles/css/Common/ProductDetail.css';
 
 class ProductDetail extends Component {
   render() {
     return (
-      <div>
-        <img
-          src="http://dummyimage.com/500x500/00c4e2/fff"
-          alt="detail"
-          align="left"
-        />
-        <h5>볼륨 + 탄력</h5>
-        <h3>큐오필 1cc</h3>
-        <h4>신사역</h4>
-        <h4>59000원</h4>
-        <h4>옵션</h4>
-        <button type="submit">찜하기</button>
-        <button type="submit">예약하기</button>
-        <h4>구매 300개</h4>
+      <div className="productDetail">
+        <div className="productDetail__category">
+          <p>홈 > 시술 상품 > 보톡스</p>
+        </div>
+        <div className="productDetail__info">
+          <div className="productDetail__info__img">
+            <img
+              src="http://dummyimage.com/480x480/00c4e2/fff"
+              alt="detail"
+              align="left"
+            />
+          </div>
+          <div className="productDetail__info__description">
+            <h5>볼륨 + 탄력</h5>
+            <h3>큐오필 1cc</h3>
+            <h4>신사역</h4>
+            <h4>59000원</h4>
+            <h4>옵션</h4>
+            <button type="submit">찜하기</button>
+            <button type="submit">예약하기</button>
+            <h4>구매 300개</h4>
+          </div>
+        </div>
       </div>
     );
   }
