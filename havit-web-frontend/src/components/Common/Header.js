ffffbebc52cf817f57157552474395bdf9981c85
JungYouTaek <jyt9319@gmail.com>
2018-02-19 14:26:08 +0900

    Add Header, Nav styles

--- havit-web-frontend/src/components/Common/Header.js
+++ havit-web-frontend/src/components/Common/Header.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { logo } from 'assets/img';
 import 'styles/css/Common/Header.css';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
-        <Link to="/login" className="header__link">
-          로그인
-        </Link>
-        <Link to="/signin" className="header__link">
-          회원가입
-        </Link>
-        <Link to="/service" className="header__link">
-          고객센터
-        </Link>
-        <Link to="/mypage/reserve" className="header__link">
-          마이페이지
-        </Link>
+        <div className="header__left">
+          <Link to="/" className="header__home">
+            <img src={logo} className="header__logo" alt="해빗 로고" />
+          </Link>
+        </div>
+        <div className="header__right">
+          <Link to="/login" className="header__link">
+            로그인
+          </Link>
+          <Link to="/signin" className="header__link">
+            회원가입
+          </Link>
+          <Link to="/service" className="header__link">
+            고객센터
+          </Link>
+          <Link to="/mypage/reserve" className="header__link">
+            마이페이지
+          </Link>
+        </div>
       </header>
     );
   }
--- havit-web-frontend/src/components/Common/Nav.js
+++ havit-web-frontend/src/components/Common/Nav.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { logo2 } from 'assets/img';
 import 'styles/css/Common/Nav.css';
 
 class Nav extends Component {
@@ -8,18 +7,18 @@ class Nav extends Component {
     return (
       <nav className="nav">
         <div className="nav__wrapper">
-          <div className="nav__top">
-            <NavLink to="/" className="nav__home">
-              <img src={logo2} className="nav__logo" alt="해빗 로고"/>
-            </NavLink>
-            <input type="text" className="nav__search"/>
-          </div>
-          <div className="nav__bottom">
-            <NavLink to="/all" className="nav__list">전체보기</NavLink>
-            <NavLink to="/skin" className="nav__list">피부시술</NavLink>
-            <NavLink to="/beauty" className="nav__list">뷰티시술</NavLink>
-            <NavLink to="/event" className="nav__list">이벤트</NavLink>
-          </div>
+          <NavLink to="/all" className="nav__list">
+            전체보기
+          </NavLink>
+          <NavLink to="/skin" className="nav__list">
+            피부시술
+          </NavLink>
+          <NavLink to="/beauty" className="nav__list">
+            뷰티시술
+          </NavLink>
+          <NavLink to="/event" className="nav__list">
+            이벤트
+          </NavLink>
         </div>
       </nav>
     );
