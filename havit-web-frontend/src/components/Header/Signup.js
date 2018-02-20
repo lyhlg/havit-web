b16a9a2ea8d88b21dfd91127a37e67f677b43ebd
dubbsong <dubbsong@gmail.com>
2018-02-20 19:46:12 +0900

    Modify signup

--- havit-web-frontend/src/components/Header/Signup.js
+++ havit-web-frontend/src/components/Header/Signup.js
@@ -22,29 +22,33 @@ class Signup extends Component {
     return (
       <div className="signup">
         <img src={logo3} className="signup__logo" alt="havit" />
-        <main className="signup__wrapper">
-          <h3 className="signup__emailword">이메일 주소</h3>
-          <input
-            type="text"
-            className="signup__emailbox"
-            placeholder="이메일@도메인"
-          />
-          <h3 className="signup__psw">비밀번호 입력</h3>
-          <input
-            type="text"
-            className="signup__pswbox"
-            placeholder="영문, 숫자로 6자 이상 입력해주세요."
-          />
-          <h3 className="signup__pswcheck">비밀번호 확인</h3>
-          <input
-            type="text"
-            className="signup__pswbox"
-            placeholder="동일하게 다시 한 번 입력해주세요."
-          />
-          <button className="signup__button">
-            <Link to="/privacy">계정 생성</Link>
-          </button>
-        </main>
+        <div className="signup__wrapper">
+          <div className="signup__tab">
+            <h3 className="signup__label">이메일 주소</h3>
+            <input
+              type="text"
+              className="signup__input"
+              placeholder="이메일@도메인"
+            />
+            <h3 className="signup__label">비밀번호 입력</h3>
+            <input
+              type="text"
+              className="signup__input"
+              placeholder="영문, 숫자로 6자 이상 입력해주세요."
+            />
+            <h3 className="signup__label">비밀번호 확인</h3>
+            <input
+              type="text"
+              className="signup__input"
+              placeholder="동일하게 다시 한 번 입력해주세요."
+            />
+            <button className="signup__button">
+              <Link to="/privacy" className="signup__btntext">
+                계정 생성
+              </Link>
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
