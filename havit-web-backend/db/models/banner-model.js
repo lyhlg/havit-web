b872112eab58581e3be5843dc0a3b2df8e406861
lyhlg <lyhlg0201@gmail.com>
2018-02-20 14:30:58 +0900

    Add Banner API

--- havit-web-backend/db/models/banner-model.js
+++ havit-web-backend/db/models/banner-model.js
@@ -2,9 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bannerSchema = new Schema({
-  totalBanner : [String],
-  skinBanner: [String],
-  beautyBanner: [String]
+  totalBanners : [String],
+  skinBanners: [String],
+  beautyBanners: [String]
 });
 
 module.exports = mongoose.model('banner', bannerSchema, 'banners');
+
+// dummy : 추후 관리자 업데이트 할 수 있도록 기능 구현 필요
+/*
+db.banners.insert({
+  totalBanners: [
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8c8"),
+    ObjectId("5a8b9323d485e3b7ca525abd"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8ca"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8cb"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8d0")
+  ],
+  skinBanners: [
+    ObjectId("5a8b9323d485e3b7ca525abb"),
+    ObjectId("5a8b9323d485e3b7ca525abe"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8cd"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8ce"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8cf")
+  ],
+  beautyBanners: [
+    ObjectId("5a8b9323d485e3b7ca525abc"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8cc"),
+    ObjectId("5a8b9323d485e3b7ca525abf"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8c7"),
+    ObjectId("5a8b9e2ae5fb2bb6ef38c8c9")
+  ]
+})
+*/
