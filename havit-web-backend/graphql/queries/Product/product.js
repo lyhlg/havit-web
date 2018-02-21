fa242c9ab457d7b72773d1da8f1af2015b494fe6
lyhlg <lyhlg0201@gmail.com>
2018-02-21 14:03:44 +0900

    Modify product parameter

--- havit-web-backend/graphql/queries/Product/product.js
+++ havit-web-backend/graphql/queries/Product/product.js
@@ -2,26 +2,27 @@ const ObjectId = require('mongodb').ObjectID;
 
 const FIND_PRODUCT = async ( params ) => {
   const [obj, args, ctx] = [...params];
-  if ( obj ) {
-    return (await ctx.product.findOne({ _id: ObjectId(obj.product) }))
-  } else {
-    if ( !(args.type && args.subType) ){
-      if (!args.limit) args.limit = 12;
-      if (!args.page) args.page = 1;
-      return await ctx.product.find()
-        .sort({ _id: -1 })
-        .skip((args.page - 1) * args.limit)
-        .limit(args.limit);
-    } else {
-      if (!args.limit) args.limit = 12;
-      if (!args.page) args.page = 1;
-      return await ctx.product
-        .find({ type: args.type, subType: args.subType })
-        .sort({ _id: -1 })
-        .skip((args.page - 1) * args.limit)
-        .limit(args.limit);
-    }
+  if (!args.limit) args.limit = 12;
+  if (!args.page) args.page = 1;
 
+  const res = async (arg) => await ctx.product
+    .find(arg)
+    .sort({ _id: -1 })
+    .skip((args.page - 1) * args.limit)
+    .limit(args.limit);
+
+  if ( obj ) {
+    return (await ctx.product.findOne());
+  } else {
+    if ( !(args.type || args.subType) ){
+      return res({});
+    } else {
+      if ( args.type && !args.subType ){
+        return res({ type: args.type });
+      } else if ( args.type && args.subType ) {
+        return res({ type: args.type, subType: args.subType });
+      }
+    }
   }
 };
 
