d78361bec8fb0e33f37e4397285aa7cd1642a298
dubbsong <dubbsong@gmail.com>
2018-02-22 18:46:12 +0900

    Edit HospitalPage

--- havit-web-frontend/src/components/HospitalPage/Balance.js
+++ havit-web-frontend/src/components/HospitalPage/Balance.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import 'styles/css/HospitalPage/Balance.css';
 
 class Balance extends Component {
   render() {
     return (
-      <div>
-        <table border="1">
-          <tr>
-            <th>기간</th>
-            <th>게시물 개수</th>
-            <th>판매 개수</th>
-            <th>-</th>
-          </tr>
-          <td>
-            <th>Wassup bro?</th>
-          </td>
-        </table>
+      <div className="uploadProduct">
+        <div className="uploadProduct__wrapper">
+          <div className="uploadProduct__tab">
+            <table border="1">
+              <tr>
+                <th>기간</th>
+                <th>게시물 개수</th>
+                <th>판매 개수</th>
+                <th>-</th>
+              </tr>
+              <td>
+                <th>Wassup bro?</th>
+              </td>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
