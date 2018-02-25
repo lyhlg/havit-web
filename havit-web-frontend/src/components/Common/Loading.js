225fe7d1cbc2fec49705166830f712c3dee8b8c6
JungYouTaek <jyt9319@gmail.com>
2018-02-25 21:26:57 +0900

    Add loading

--- havit-web-frontend/src/components/Common/Loading.js
+++ havit-web-frontend/src/components/Common/Loading.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import 'styles/css/Common/Loading.css';
 
 class Loading extends Component {
   render() {
-    return <div>로딩이다 기다룟</div>;
+    return (
+      <div className="loading">
+        <div className="loading__text">
+          <h2>로딩중 입니다</h2>
+        </div>
+        <div className="loading__spinner">
+          <div className="container">
+            <div className="holder">
+              <div className="box" />
+            </div>
+            <div className="holder">
+              <div className="box" />
+            </div>
+            <div className="holder">
+              <div className="box" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
