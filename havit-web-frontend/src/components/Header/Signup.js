33a81e6736c5d64d19102ec6821820de80522ca6
JungYouTaek <jyt9319@gmail.com>
2018-02-26 16:08:36 +0900

    Edit signup

--- havit-web-frontend/src/components/Header/Signup.js
+++ havit-web-frontend/src/components/Header/Signup.js
@@ -9,6 +9,40 @@ class Signup extends Component {
     this.state = {
       arr: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
       month: 31,
+      clinics: [
+        '여드름',
+        '주름',
+        '기미',
+        '잡티/주근깨',
+        '흉터',
+        '모공',
+        '홍조',
+        '제모',
+        '튼살',
+        '다크써클',
+        '수분부족',
+        '피부톤',
+        '노화',
+        '탈모',
+        ' 점/검버섯',
+        '볼꺼짐',
+        '문신제거',
+        '기타고민',
+      ],
+      regions: [
+        '강남구',
+        '강동구',
+        '강북구',
+        '강서구',
+        '관악구',
+        '광진구',
+        '노원구',
+        '도봉구',
+        '마포구',
+        '서초구',
+        '서대문구',
+        '송파구',
+      ],
     };
     this.changeMonth = this.changeMonth.bind(this);
     this.submitUserInfo = this.submitUserInfo.bind(this);
@@ -24,9 +58,7 @@ class Signup extends Component {
     let data = [
       document.getElementById('email').value,
       document.getElementById('name').value,
-      document.getElementById('txtMobile1').value +
-        document.getElementById('txtMobile2').value +
-        document.getElementById('txtMobile3').value,
+      document.getElementById('txtMobile').value,
       document.getElementById('birthday1').value +
         document.getElementById('birthday2').value +
         document.getElementById('birthday3').value,
@@ -73,185 +105,152 @@ class Signup extends Component {
               className="signup__input"
               placeholder="동일하게 다시 한 번 입력해주세요."
             />
-            <h3 className="signup__label">이름</h3>
-            <input
-              id="name"
-              type="text"
-              className="signup__input"
-              placeholder="실명 한글 이름을 입력해주세요."
-            />
-            <h3 className="signup__label">핸드폰 번호</h3>
-            <input
-              type="text"
-              id="txtMobile"
-              size="4"
-              className="signup__phone"
-              placeholder="- 빼고 숫자만 입력"
-            />
-            <h3 className="signup__label">생년 월 일</h3>
-            <select id="birthday1" className="signup__box1">
-              <option selected disabled>
-                년
-              </option>
-              {Array(81)
-                .fill(1930)
-                .map((a, i) => {
-                  return <option key={i}>{a + i}</option>;
-                })}
-            </select>
-            <select
-              id="birthday2"
-              className="signup__box2"
-              onChange={this.changeMonth}
-            >
-              <option selected disabled>
-                월
-              </option>
-              {Array(12)
-                .fill(1)
-                .map((a, i) => {
-                  return (
-                    <option key={i}>
-                      {(a + i).toString().length < 2 ? '0' + (a + i) : a + i}
-                    </option>
-                  );
-                })}
-            </select>
-            <select id="birthday3" className="signup__box2">
-              <option selected disabled>
-                일
-              </option>
-              {Array(this.state.month)
-                .fill(1)
-                .map((a, i) => {
-                  return (
-                    <option key={i}>
-                      {(a + i).toString().length < 2 ? '0' + (a + i) : a + i}
-                    </option>
-                  );
-                })}
-            </select>
-            <h3 className="signup__label">성별</h3>
-            <input
-              name="gender"
-              id="gender"
-              type="radio"
-              className="signup__checkbox"
-              value="여자"
-            />
-            <label className="signup__text">여자</label>
-            <input
-              name="gender"
-              id="gender"
-              type="radio"
-              className="signup__checkbox"
-              value="남자"
-            />
-            <label className="signup__text">남자</label>
+            <div className="signup__box">
+              <div className="signup__box3">
+                <h3 className="signup__label">이름</h3>
+                <input
+                  id="name"
+                  type="text"
+                  className="signup__input"
+                  placeholder="실명 한글 이름을 입력해주세요."
+                />
+              </div>
+              <div className="signup__box4">
+                <h3 className="signup__label signup__gender">성별</h3>
+                <label className="signup__labelText">
+                  <input
+                    name="gender"
+                    id="gender"
+                    type="radio"
+                    className="signup__checkbox"
+                    value="여자"
+                  />
+                  여자
+                </label>
+                <label className="signup__labelText">
+                  <input
+                    name="gender"
+                    id="gender"
+                    type="radio"
+                    className="signup__checkbox"
+                    value="남자"
+                  />
+                  남자
+                </label>
+              </div>
+            </div>
+            <div className="signup__box">
+              <div className="signup__box3">
+                <h3 className="signup__label">핸드폰 번호</h3>
+                <input
+                  type="text"
+                  id="txtMobile"
+                  size="4"
+                  className="signup__input"
+                  placeholder="- 빼고 숫자만 입력"
+                />
+              </div>
+              <div className="signup__box4">
+                <h3 className="signup__label">생년 월 일</h3>
+                <select
+                  id="birthday1"
+                  defaultValue="1990"
+                  className="signup__box1"
+                >
+                  {Array(81)
+                    .fill(1930)
+                    .map((a, i) => {
+                      return <option key={i}>{a + i}</option>;
+                    })}
+                </select>
+                <select
+                  id="birthday2"
+                  className="signup__box2"
+                  onChange={this.changeMonth}
+                >
+                  <option selected disabled>
+                    월
+                  </option>
+                  {Array(12)
+                    .fill(1)
+                    .map((a, i) => {
+                      return (
+                        <option key={i}>
+                          {(a + i).toString().length < 2
+                            ? '0' + (a + i)
+                            : a + i}
+                        </option>
+                      );
+                    })}
+                </select>
+                <select id="birthday3" className="signup__box2">
+                  <option selected disabled>
+                    일
+                  </option>
+                  {Array(this.state.month)
+                    .fill(1)
+                    .map((a, i) => {
+                      return (
+                        <option key={i}>
+                          {(a + i).toString().length < 2
+                            ? '0' + (a + i)
+                            : a + i}
+                        </option>
+                      );
+                    })}
+                </select>
+              </div>
+            </div>
             <h3 className="signup__label">관심 지역</h3>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="강남구"
-            />
-            <label className="signup__text">강남구</label>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="서초구"
-            />
-            <label className="signup__text">서초구</label>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="중구"
-            />
-            <label className="signup__text">중구</label>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="성동구"
-            />
-            <label className="signup__text">성동구</label>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="광진구"
-            />
-            <label className="signup__text">광진구</label>
-            <input
-              name="likeArea"
-              type="checkbox"
-              className="signup__checkbox"
-              value="영등포구"
-            />
-            <label className="signup__text">영등포구</label>
+            {this.state.regions.map((region, i) => {
+              return (
+                <div className="signup__checkboxWrapper" key={i}>
+                  <label className="signup__text">
+                    <input
+                      name="likeArea"
+                      type="checkbox"
+                      className="signup__checkbox"
+                      value={region}
+                    />
+                    {region}
+                  </label>
+                </div>
+              );
+            })}
             <h3 className="signup__label">관심 부위</h3>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="피부"
-            />
-            <label className="signup__text">피부</label>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="여드름"
-            />
-            <label className="signup__text">여드름</label>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="리프팅"
-            />
-            <label className="signup__text">리프팅</label>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="제모"
-            />
-            <label className="signup__text">제모</label>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="반영구"
-            />
-            <label className="signup__text">반영구</label>
-            <input
-              name="likePoint"
-              type="checkbox"
-              className="signup__checkbox"
-              value="미용주사"
-            />
-            <label className="signup__text">미용주사</label>
-            <hr />
-            <h3 className="signup__label">병원코드</h3>
+            {this.state.clinics.map((clinic, i) => {
+              return (
+                <div className="signup__checkboxWrapper" key={i}>
+                  <label className="signup__text">
+                    <input
+                      name="likePoint"
+                      type="checkbox"
+                      className="signup__checkbox"
+                      value={clinic}
+                    />
+                    {clinic}
+                  </label>
+                </div>
+              );
+            })}
+            <h3 className="signup__label">병원 코드</h3>
             <input
               id="code"
               type="text"
               className="signup__input"
-              placeholder="병원코드"
+              placeholder="병원 관리자 전용입니다"
             />
-            <h6 className="signup__subtext">병원 관리자 전용입니다</h6>
-            <hr />
+            <hr className="signup__hr" />
             <h6 className="signup__info">
               회원가입 시 이용약관, 개인정보 수집 및 이용에 동의로 간주합니다.
             </h6>
-            <button onClick={this.submitsignup} className="signup__button">
-              <Link to="/signup" className="signup__btntext">
-                회원가입
-              </Link>
-            </button>
+            <div className="signup__btn">
+              <button onClick={this.submitsignup} className="signup__button">
+                <Link to="/signup" className="signup__btntext">
+                  회원가입
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
