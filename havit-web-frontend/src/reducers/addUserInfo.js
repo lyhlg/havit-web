e2eac7d02327bb69daf35050d5db369cd123e433
JungYouTaek <jyt9319@gmail.com>
2018-03-06 17:54:34 +0900

    Edit Login

--- havit-web-frontend/src/reducers/addUserInfo.js
+++ havit-web-frontend/src/reducers/addUserInfo.js
@@ -9,7 +9,7 @@ const userInfo = (state = initialState, action) => {
     case types.SUCCESS_ADD_USERINFO:
       return {
         ...state,
-        newUserInfo: action.newUserInfo.data.addUserInfo,
+        newUserInfo: action.newUserInfo,
       };
     case types.REQUEST_ADD_USERINFO:
     default:
