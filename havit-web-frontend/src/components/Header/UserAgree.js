d3da8b6e166bd3c7617cdc1d53a4ed2a77480cd9
dubbsong <dubbsong@gmail.com>
2018-03-09 23:57:56 +0900

    Edit UserAgree

--- havit-web-frontend/src/components/Header/UserAgree.js
+++ havit-web-frontend/src/components/Header/UserAgree.js
@@ -6,11 +6,16 @@ class UserAgree extends Component {
   render() {
     return (
       <div className="userAgree">
-        <div className="userAgree__title">
-          <p>havit 이용약관</p>
+        <div>
+          <p className="userAgree__title">havit 이용약관</p>
         </div>
-        <div className="userAgree__content">
-          <p>이용약관 1조 목적</p>
+        <div className="userAgree__tab">
+          <p className="userAgree__content">이용약관 1조 목적</p>
+          <p>
+            이 약관은 ㈜Nomad Donut이 운영하는 애플리케이션 havit 시스템을
+            이용하는 ‘havit’ MOU체결 병의원(이하 ‘제휴병의원’)과 이용자의
+            권리•의무 및 책임사항을 규정함을 목적으로 합니다.
+          </p>
         </div>
       </div>
     );
