5ebddf575e4f2a88904347d80d50b793501bc9f7
lyhlg <lyhlg0201@gmail.com>
2018-03-12 16:18:39 +0900

    Review Change ordering

--- havit-web-backend/graphql/queries/Review/review.js
+++ havit-web-backend/graphql/queries/Review/review.js
@@ -2,12 +2,16 @@ const ObjectId = require("mongodb").ObjectID;
 
 const FIND_REVIEW = async params => {
   const [obj, args, ctx] = [...params];
+  console.log(args);
   if (obj) {
-    return obj.reviews.map(async item => {
+    console.log("obj");
+    const reviews = obj.reviews.map(async item => {
       return await ctx.review.findOne({ _id: ObjectId(item) });
     });
+    return await Promise.all(reviews).then(res => res.reverse());
   } else {
-    return await ctx.review.find(args).sort({ _id: -1});
+    console.log("args");
+    return await ctx.review.find(args).sort({ _id: -1 });
   }
 };
 
